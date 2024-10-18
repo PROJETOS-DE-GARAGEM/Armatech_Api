635691640f2b0cc216a0270e1a8039f8dc1a5232
Ruan <rugcosta1234@gmail.com>
2024-10-18 15:10:42 -0300

    Feito a conexão com o banco de dados

--- Armatech/src/main/java/com/estaciojava/Armatech/model/Usuario.java
+++ Armatech/src/main/java/com/estaciojava/Armatech/model/Usuario.java
@@ -14,54 +14,54 @@ public class Usuario {
     private String nome;
     private String email;
 
-
-import javax.persistence.*;
-
-@entity //Indica que essa classe é uma entidade JPA, que será mapeada para uma tabela no BD.
-@table(name = "usuarios")// Define o nome da tabela no banco de dados.
-
-public class Usuario {
-    //Indicam que o campo id será a chave primária gerada automaticamente.
-    @id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-
-    private Long id;
-
-    private String nome;
-    private String email;
-    private String senha;
-
-
-    //Getters e Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
+//import javax.persistence.*;
+
+//@entity //Indica que essa classe é uma entidade JPA, que será mapeada para uma tabela no BD.
+//@table(name = "usuarios")// Define o nome da tabela no banco de dados.
+
+//public class Usuario {
+//    //Indicam que o campo id será a chave primária gerada automaticamente.
+//    @id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//
+//    private Long id;
+//
+//    private String nome;
+//    private String email;
+//    private String senha;
+//
+//
+//    //Getters e Setters
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getNome() {
+//        return nome;
+//    }
+//
+//    public void setNome(String nome) {
+//        this.nome = nome;
+//    }
+//
+//    public String getSenha() {
+//        return senha;
+//    }
+//
+//    public void setSenha(String senha) {
+//        this.senha = senha;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//}
--- Armatech/src/main/java/com/estaciojava/Armatech/repository/UsuarioRepository.java
+++ Armatech/src/main/java/com/estaciojava/Armatech/repository/UsuarioRepository.java
@@ -1,9 +1,10 @@
 package com.estaciojava.Armatech.repository;
 import com.estaciojava.Armatech.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 }
