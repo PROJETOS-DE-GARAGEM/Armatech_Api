2437b9d9e3ee43c4254bae92858c235d9c3d0ece
Isaac Gomes <isaacgomes932@gmail.com>
2024-10-23 16:02:29 -0300

    Modificando para explicitar a referencia

--- Armatech/src/main/java/com/estaciojava/Armatech/model/Lancamento.java
+++ Armatech/src/main/java/com/estaciojava/Armatech/model/Lancamento.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
+
 
 @Getter
 @Setter
@@ -21,8 +23,10 @@ public class Lancamento {
     private Integer tipo;
     private Integer quantidade;
 
+    // Referência à entidade Produto OBS: Preciso de Produtos feito para que tenha essa relação
+    @JoinColumn(name = "idProduto", referencedColumnName = "id") //Somente para explicitar que a PK é 'idProduto'
     @ManyToOne // Estabelece que vários lançamentos podem referenciar um único produto
-    private Produto produto; // Referência à entidade Produto OBS: Preciso de Produtos feito para que tenha essa relação
+    private Produto produto;
 
     /*Só para visualizar melhor como está no banco
 
