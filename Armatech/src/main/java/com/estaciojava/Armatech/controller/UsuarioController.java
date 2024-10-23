ef8efab447c50537f65036f5f5bf6a33a5427b0b
Ruan <rugcosta1234@gmail.com>
2024-10-23 09:09:09 -0300

    Atualizando a lógica de autenticação de login e senha

--- Armatech/src/main/java/com/estaciojava/Armatech/controller/UsuarioController.java
+++ Armatech/src/main/java/com/estaciojava/Armatech/controller/UsuarioController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/usuarios")
+@RequestMapping("/usuario")
 public class UsuarioController {
     @GetMapping
     public ResponseEntity<String> getUsuario(){
---
+++ Armatech/src/main/java/com/estaciojava/Armatech/infra/cors/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.estaciojava.Armatech.infra.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://192.168.73.3:8081")
+                .allowedMethods("GET", "POST");
+
+    }
+}
